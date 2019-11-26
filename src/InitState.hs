7c7f1666d9e1f5dba67c6bf5dc35165034769ae1
Jon Harder <jonharder6@gmail.com>
2019-11-26 17:23:07 -0600

    fix int parse errors breaking game

--- src/InitState.hs
+++ src/InitState.hs
@@ -4,12 +4,17 @@ where
 
 import Control.Monad (forM_)
 import System.IO (stdout, hFlush)
+import Text.Read (readMaybe)
 
 import Stories.Types (Story(..))
 
 import qualified Stories.DungeonsOfDoom as DungeonsOfDoom
 import qualified Stories.Mangos as Mangos
 import qualified Stories.CryoSleep as CryoSleep
+
+
+readInt :: IO (Maybe Int)
+readInt = readMaybe <$> getLine
 
 
 pickStory :: IO Story
@@ -23,5 +28,9 @@ pickStory = do
     putStrLn $ show index ++ ": " ++ title story
   putStr "enter number of story: "
   hFlush stdout
-  input <- readLn :: IO Int
-  maybe pickStory return (lookup input options)
+  input <- readInt
+  case input of
+    Just i ->
+      maybe pickStory return (lookup i options)
+    Nothing ->
+      pickStory
